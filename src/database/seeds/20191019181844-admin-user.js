a40aab392e7fe7b472df9411f84666c5b5ec4de9
jeln666 <je.luis.nascimento@hotmail.com>
2019-10-19 17:13:34 -0300

    Correção do campo isAdmin na tabela uers

--- src/database/seeds/20191019181844-admin-user.js
+++ src/database/seeds/20191019181844-admin-user.js
@@ -9,7 +9,7 @@ module.exports = {
           name: 'Administrador',
           email: 'admin@gympoint.com',
           password_hash: bcrypt.hashSync('123456', 8),
-          admin: true,
+          isAdmin: true,
           created_at: new Date(),
           updated_at: new Date(),
         },

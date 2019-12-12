e0f34e5a82294c740d0eb9f7e26d9260cbfcc9d7
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2019-12-12 20:04:26 -0200

    Configurando o redux para autenticação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 import GlobalStyle from '~/styles/global';
 
 import './config/ReactotronConfig';
 import Routes from './Routes';
 import history from './services/history';
+import { store, persistor } from './store';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-      <ToastContainer autoClose={3000} />
-    </Router>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+          <ToastContainer autoClose={3000} />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 

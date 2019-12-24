ef2c4657122d0d8d50db92d5eb74e4d63224b994
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2019-12-24 11:12:28 -0200

    Ajustes no checkin

--- mobile/src/pages/Checkins/index.js
+++ mobile/src/pages/Checkins/index.js
@@ -25,41 +25,76 @@ export default function Checkins() {
   const student = useSelector(state => state.student.student);
 
   const [checkins, setChekins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [err, setErr] = useState(false);
 
   const countCheckin = useMemo(() => (!checkins ? 0 : checkins.length), [
     checkins,
   ]);
 
+  const loadText = useMemo(() => {
+    let text = `${student.name}, você ainda não fez nenhum check-in!`;
+
+    if (err && !loading) {
+      text = 'Ops, não conseguimos carregar seus check-ins.';
+    }
+
+    if (loading) {
+      text = 'Carregando...';
+    }
+
+    return text;
+  }, [err, loading, student.name]);
+
   async function loadCheckins() {
-    const response = await api.get(`students/${student.id}/checkins`);
-    setChekins(
-      response.data.map(checkin => ({
-        ...checkin,
-        dateFormatted: formatRelative(
-          parseISO(checkin.created_at),
-          new Date(),
-          {
-            locale: pt,
-            addSuffix: true,
-          }
-        ),
-      }))
-    );
+    try {
+      setLoading(true);
+      const response = await api.get(`students/${student.id}/checkins`);
+      setChekins(
+        response.data.map(checkin => ({
+          ...checkin,
+          dateFormatted: formatRelative(
+            parseISO(checkin.created_at),
+            new Date(),
+            {
+              locale: pt,
+              addSuffix: true,
+            }
+          ),
+        }))
+      );
+    } catch (error) {
+      setErr(true);
+      console.tron.error(error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   useEffect(() => {
     loadCheckins();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [student.id]);
+  }, []);
 
   async function handleSubmit() {
     try {
-      const response = await api.post(`students/${student.id}/checkins`);
-      console.tron.log(response);
+      await api.post(`students/${student.id}/checkins`);
+
+      Alert.alert('GymPoint', 'Check-in realizado com sucesso!');
+
       loadCheckins();
     } catch (error) {
-      console.tron.error(error.code);
-      Alert.alert('GymPOint', 'Limite de Check-ins alcançado');
+      console.tron.error(error);
+      console.tron.error(error.response);
+      console.tron.error(error.response);
+      if (error.response && error.response.status === 401) {
+        Alert.alert('GymPoint', 'Limite de Check-ins alcançado');
+      } else {
+        Alert.alert(
+          'GymPoint',
+          'Ops, ocorreu um problema ao realizar o check-in, tente novamente mais tarde.'
+        );
+      }
     }
   }
 
@@ -82,9 +117,7 @@ export default function Checkins() {
             )}
           />
         ) : (
-          <EmptyList>
-            {student.name}, você ainda não fez nenhum check-in!
-          </EmptyList>
+          <EmptyList>{loadText}</EmptyList>
         )}
       </Content>
     </Container>
